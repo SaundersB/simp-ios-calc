7fde04fdd9b143ee1dd50a76075d9682d366ea1d
SaundersB <speakeasys@gmail.com>
2015-06-22 15:47:54 -0700

    Addition of CalculatorBrain class.

---
+++ Calculator/CalculatorBrain.swift
@@ -0,0 +1,42 @@
+//
+//  CalculatorBrain.swift
+//  Calculator
+//
+//  Created by Brandon Saunders on 6/22/15.
+//  Copyright (c) 2015 Brandon Saunders. All rights reserved.
+//
+
+import Foundation
+
+class CalculatorBrain
+{
+    enum Op {
+        case Operand(Double)
+        case Operation(String,Double -> Double)
+        case BinaryOperation(String, (Double,Double) -> Double)
+        
+    }
+    
+    var opStack = [Op]()
+    
+    var knownOps = [String:Op]()
+    
+    init() {
+        knownOps ["×"] = Op.BinaryOperation("×") {$0 * $1}
+        knownOps ["÷"] = Op.BinaryOperation("÷") {$1 / $0}
+        knownOps ["+"] = Op.BinaryOperation("+") {$0 + $1}
+        knownOps ["−"] = Op.BinaryOperation("-") {$1 - $0}
+        //knownOps ["√"] = Op.UnaryOperation("√",sqrt)
+    
+        
+    }
+    
+    
+    func pushOperand(operand: Double) {
+        opStack.append(Op.Operand(operand))
+    }
+    
+    func performOperation(symbol: String) {
+        
+    }
+}
