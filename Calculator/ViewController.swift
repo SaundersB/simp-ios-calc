16a9daf68d7787ec9365a06d91e01584c0155555
SaundersB <speakeasys@gmail.com>
2015-06-22 12:22:30 -0700

    Fixed an issue with UILabel. Calculator appends the numbers to the display properly.

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -10,16 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+    @IBOutlet weak var display: UILabel!
+
+
+    @IBAction func appendDigit(sender: UIButton) {
+        let digit = sender.currentTitle!
+        display.text = display.text! + digit
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
